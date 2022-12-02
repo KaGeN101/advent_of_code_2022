42ba450ef04073be205ff1d49bcb92c0d4c504e8
Johann Eloff <johann.eloff@saltpay.co>
2022-12-02 10:53:33 +0200

    Added part II

--- day2/tournament_again.rb
+++ day2/tournament_again.rb
@@ -11,9 +11,24 @@
     'C Z' => 6, 
 }
 
+combinationsII = { 
+    'A X' => 3, 
+    'A Y' => 4, 
+    'A Z' => 8, 
+    'B X' => 1, 
+    'B Y' => 5, 
+    'B Z' => 9, 
+    'C X' => 2, 
+    'C Y' => 6, 
+    'C Z' =>  7, 
+};
+
 total = 0
+totalII = 0
 File.foreach("input.txt") do |line| 
     total += combinations[line.strip]
+    totalII += combinationsII[line.strip]
 end
 
 puts total
+puts totalII
