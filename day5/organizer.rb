e7ae5dc583592665a44d433a8643f94caa33e3a4
Johann Eloff <johann.eloff@saltpay.co>
2022-12-05 10:42:04 +0200

    Part 1 solved

--- day5/organizer.rb
+++ day5/organizer.rb
@@ -13,7 +13,6 @@ index = 0
 state_line.each do |r|
     while index < r.length
         crate = r[index ... index+3]
-        puts crate
         if crate.include? '['
             state.add Crate.new(row, col, crate[1])
         end
@@ -25,4 +24,28 @@ state_line.each do |r|
     index = 0
 end  
 
+puts state.to_s
+puts "Total Crates: #{state.total_crates}"
+puts "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+
+# Parse All the instructions
+instructions = []
+File.foreach("input.txt") do |line|
+    splitted = line.strip.split(' ')
+    instructions << Instruction.new(splitted[1].to_i, splitted[3].to_i, splitted[5].to_i)
+end 
+
+# Apply instructions
+number = 0
+instructions.each do |instruction|
+    number += 1
+    puts "Instruction: #{number}"
+    puts "Total Crates: #{state.total_crates}"
+    if number == 11
+        puts state.yard[0][7].value
+    end
+    state.move instruction
+    puts state.to_s
+    puts "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+end
 puts state.to_s
--- day5/state.rb
+++ day5/state.rb
@@ -11,21 +11,75 @@ class Crate
 
 end
 
+class Instruction
+
+    attr_reader :count, :from, :to
+
+    def initialize(move, from, to) 
+        @count = move
+        @from = from
+        @to = to
+    end
+
+end
+
 class State
 
+    attr_reader :yard
+
     def initialize()
-        @yard = Array.new(8) { Array.new(9)} # rows | coloumns
+        #TODO: replace magic number sizes and lengths with constants
+        @width = 50
+        @height = 50
+        @yard = Array.new(@height) { Array.new(@width)} # rows | coloumns
     end
 
     def add crate
-        @yard[crate.x][crate.y] = crate
+        if crate
+            @yard[crate.x][crate.y] = crate
+        end
     end
+
+    def move instruction
+        col_from = instruction.from - 1
+        row_top_from = get_row_top col_from
+        to_move = []
+        (row_top_from).downto(row_top_from-instruction.count+1).each do |row|
+             to_move << @yard[row][col_from]
+             @yard[row][col_from] = nil
+        end
+        col_to = instruction.to - 1
+        row_top_to = get_row_top col_to 
+        p instruction
+        puts row_top_to
+        if row_top_to == -1
+            row_top_to = 0
+        else 
+            row_top_to += 1
+        end
+        to_move.each do |crate|
+            @yard[row_top_to][col_to] = crate
+            row_top_to += 1
+        end
+    end
+
+    def total_crates
+        total = 0
+        (0..@height-1).each do |row|
+            (0..@width-1).each do |col|
+                if @yard[row][col] 
+                    total += 1
+                end
+            end
+        end
+        return total
+    end 
 
     def to_s
         stack = []
         one_row = ""
-        (0..7).each do |row|
-            (0..8).each do |col|
+        (0..@height-1).each do |row|
+            (0..@width-1).each do |col|
                 if @yard[row][col] 
                     one_row += "[#{@yard[row][col].value}]"
                 else 
@@ -37,10 +91,20 @@ class State
             stack << one_row
             one_row = ""
         end
-        p stack.reverse
-        #stack.reverse.each do |row|
-        #    putc row
-        #end
+        stack.reverse
     end 
+
+    private 
+
+    def get_row_top col
+        row_top = -1
+        (@height-1).downto(0).each do |row|
+            if @yard[row][col]
+                row_top = row
+                break
+            end
+        end
+        return row_top
+    end
 
 end
