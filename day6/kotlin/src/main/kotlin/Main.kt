22dd26d32fe72ca8c5b627fb5a50ce294268b0c7
Johann Eloff <johann.eloff@saltpay.co>
2022-12-06 17:04:08 +0200

    Added kotlin implementation added

--- day6/kotlin/src/main/kotlin/Main.kt
+++ day6/kotlin/src/main/kotlin/Main.kt
@@ -1,6 +1,12 @@
+import java.io.File
 
 fun main(args: Array<String>) {
     val calculate: Calculate = Calculate()
+
+    val lines = File("src/main/resources/input.txt").readLines()
+    val inputData = lines[0].strip()
+    println("Part 1: " + calculate.calculate(inputData, 4))
+    println("Part 2: " + (calculate.calculate(inputData, 14) - 1) )
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
