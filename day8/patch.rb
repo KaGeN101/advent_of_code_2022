6b209f7794e40fec269216089a8826f942fd9f7e
Johann Eloff <johann.eloff@saltpay.co>
2022-12-08 15:10:05 +0200

    Algorithm completely implemeted for sample

--- day8/patch.rb
+++ day8/patch.rb
@@ -1,10 +1,49 @@
 
 class Tree
 
-    def initialize(row, col, height)
+    attr_reader :row, :col, :height, :is_edge, :is_visible, :up, :right, :down, :left
+
+    def initialize(row, col, height, is_edge)
         @row = row
         @col = col
         @height = height
+        @is_edge = is_edge   
+    end
+
+    def to_s
+        @height.to_s
+    end
+
+    def set_around up, right, down, left
+        @has_set = true
+        @up = up
+        @right = right
+        @down = down
+        @left = left
+    end
+
+    def set_is_visible visible
+        @is_visible = visible
+    end
+    
+    def print
+        out = ""
+        if @is_edge
+            out = "I am na edge don't have around and can be seen :)!"
+            puts out
+            return out
+        end
+        if @has_set
+            row_a = " #{@up.to_s} \n"  
+            row_b = "#{@left.to_s}#{@height.to_s}#{@right.to_s}\n"
+            row_c = " #{@down.to_s} \n"
+            out = row_a + row_b + row_c
+            puts out
+        else
+            out = "set_around before calling print!"
+            puts out
+        end  
+        return out
     end
 
 end
@@ -27,12 +66,85 @@ class Patch
         lines.each do |line|
             line = line.strip
             line.each_char do |char|
-                @patch[row][col] = char
+                edge =  col == 0 || row == 0 || col == @width-1 || row == @height-1 
+                @patch[row][col] = Tree.new(row, col, char.to_i, edge)
                 col += 1
             end
             col = 0
             row += 1
         end
+    end
+
+    def discover row, col
+        tree = @patch[row][col]
+        if !tree.is_edge
+            tree.set_around @patch[row-1][col], @patch[row][col+1], @patch[row+1][col], @patch[row][col-1]
+        end
+    end
+
+    def walk
+        (0..@height-1).each do |r|
+            (0..@width-1).each do |c|
+                discover(r, c)
+            end
+        end
+
+    end
+
+    def get_total_edge
+        total = 0
+        (0..@height-1).each do |r|
+            (0..@width-1).each do |c|
+                if @patch[r][c].is_edge
+                    total += 1
+                end
+            end
+        end
+        return total
+    end
+
+    def determine_visible
+        #@is_visible = @up.height < @height || @right.height < @height || @down.height < @height || @left.height < @height
+        # Just focus on the middle block
+        (1..@height-2).each do |r|
+            (1..@width-2).each do |c|
+                tree = @patch[r][c]
+                vissible_up = tree.up.height < tree.height
+                vissible_right = tree.right.height < tree.height
+                vissible_down = tree.down.height < tree.height
+                vissible_left = tree.left.height < tree.height
+                if !vissible_up && !vissible_right && !vissible_down && !vissible_left
+                    next
+                end
+                if vissible_up
+                    vissible_up = check_to_edge_row -1, tree
+                end
+                if vissible_right
+                    vissible_up = check_to_edge_col 1, tree
+                end
+                if vissible_down
+                    vissible_up = check_to_edge_row 1, tree
+                end
+                if vissible_right
+                    vissible_up = check_to_edge_col -1, tree
+                end
+                vissilbe = vissible_up || vissible_right || vissible_down || vissible_left
+                tree.set_is_visible vissilbe
+            end
+        end
+    end 
+
+    def get_total_visible
+        determine_visible
+        total = 0
+        (0..@height-1).each do |r|
+            (0..@width-1).each do |c|
+                if @patch[r][c].is_visible
+                    total += 1
+                end
+            end
+        end
+        return total
     end
 
     def draw (row, col, size)
@@ -42,13 +154,51 @@ class Patch
                 if @patch[r][c] == nil
                     patch_st += 'n'
                 else
-                    patch_st += @patch[r][c]
+                    patch_st += @patch[r][c].to_s
                 end
             end
             patch_st += "\n"
         end
         puts patch_st
         return patch_st
+    end
+
+    #private 
+
+    def check_to_edge_row direction, tree
+        if direction == -1
+            (tree.row.downto(0)).each do |row|
+                if @patch[row][tree.col].height > tree.height
+                    return false
+                end
+            end
+        end 
+        if direction == 1
+            (tree.row.upto(@height-1)).each do |row|
+                if @patch[row][tree.col].height > tree.height
+                    return false
+                end
+            end
+        end 
+        return true
+    end
+
+    def check_to_edge_col direction, tree
+        if direction == -1
+            (tree.col.downto(0)).each do |col|
+                if @patch[tree.row][col].height > tree.height
+                    return false
+                end
+            end
+        end
+        if direction == 1
+            (tree.col..@width-1).each do |col|
+                if @patch[tree.row][col].height > tree.height
+                    return false
+                end
+            end
+        end
+        return true
     end
 
 end 
--- day8/test_patch.rb
+++ day8/test_patch.rb
@@ -3,18 +3,45 @@ require "test/unit"
  
 class TestPatch < Test::Unit::TestCase
 
-    
- 
   def test_patch
     sample_patch = "30373\n25512\n65332\n33549\n35390"
     patch = Patch.new(5, 5)
     patch.parse(sample_patch.split("\n"))
     #patch.draw(0, 0, 5)
-    assert_equal("7", patch.get(0, 3))
-    assert_equal("1", patch.get(1, 3))
-    assert_equal("6", patch.get(2, 0))
-    assert_equal("4", patch.get(3, 3))
-    assert_equal("9", patch.get(4, 3))
+    assert_equal("7", patch.get(0, 3).to_s)
+    assert_equal(0, patch.get(0, 3).row)
+    assert_equal(3, patch.get(0, 3).col)
+    assert_equal(true, patch.get(0, 3).is_edge)
+    assert_equal(false, patch.get(1, 1).is_edge)
+    assert_equal("1", patch.get(1, 3).to_s)
+    assert_equal(false, patch.get(1, 3).is_edge)
+    assert_equal(true, patch.get(1, 4).is_edge)
+    assert_equal("6", patch.get(2, 0).to_s)
+    assert_equal(true, patch.get(2, 0).is_edge)
+    assert_equal(false, patch.get(2, 2).is_edge)
+    assert_equal("4", patch.get(3, 3).to_s)
+    assert_equal("9", patch.get(4, 3).to_s)
+  end
+
+  def test_surroundings
+    sample_patch = "30373\n25512\n65332\n33549\n35390"
+    patch = Patch.new(5, 5)
+    patch.parse(sample_patch.split("\n"))
+    patch.walk
+    patch.determine_visible
+    assert_equal(true, patch.get(1, 2).is_visible)
+    assert_equal(false, patch.get(3, 3).is_visible)
+  end
+
+  def test_total 
+    sample_patch = "30373\n25512\n65332\n33549\n35390"
+    patch = Patch.new(5, 5)
+    patch.parse(sample_patch.split("\n"))
+    patch.walk
+    assert_equal(16, patch.get_total_edge)
+    assert_equal(5, patch.get_total_visible)
+    # Test total visible
+    assert_equal(21, patch.get_total_edge + patch.get_total_visible)
   end
  
 end
